3ef98fb648b8641d77d44de2fed63e481a14ccbc
osohyun0224 <osohyun0224@naver.com>
2023-08-24 00:53:31 +0900

    Feat: 메인페이지 메인 타이틀 최종 디자인 반영

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,62 +1,36 @@
 import Header from "../components/Header";
-import CategoryButton from "../components/CategoryButton";
-import CourseCard from "../components/CourseCard"; 
 import styled from 'styled-components';
+import dumbbell from "../assets/images/dumbbell.png";
 
-const RecommendedTitle = styled.h1`
+const Background = styled.div`
+  width: 100%;
+  height: 250px;
+  background-color: #14F2C6;
+  position: relative;
+  margin-top: 20px;
+`;
+
+const DumbbellImage = styled.img`
+  position: absolute; 
+  bottom: 80px;
+  left: 118px;
+`;
+
+const ProgramText = styled.p`
+  position: absolute;
   font-size: 50px;
-  font-weight: bold;
-  margin-left: 200px;
+  bottom: 20px; 
+  left: 118px;
 `;
-
-const CourseWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 20px;
-  margin: 40px auto; 
-  max-width: 940px;
-`;
-
 
 const MainPage = () => {
   return (
     <div>
       <Header />
-      <CategoryButton />
-      <RecommendedTitle>추천 코스</RecommendedTitle>
-      <CourseWrapper>
-        <CourseCard 
-          image={null} 
-          title="거북목 탈출코스"
-          time="총 01시간 21분"
-        />
-        <CourseCard 
-          image={null} 
-          title="거북목 탈출코스"
-          time="총 01시간 21분"
-        />
-        <CourseCard 
-          image={null} 
-          title="거북목 탈출코스"
-          time="총 01시간 21분"
-        />
-        <CourseCard 
-          image={null} 
-          title="거북목 탈출코스"
-          time="총 01시간 21분"
-        />
-        <CourseCard 
-          image={null} 
-          title="거북목 탈출코스"
-          time="총 01시간 21분"
-        />
-        <CourseCard 
-          image={null} 
-          title="거북목 탈출코스"
-          time="총 01시간 21분"
-        />
-      </CourseWrapper>
+      <Background>
+        <DumbbellImage src={dumbbell} alt="Dumbbell" />
+        <ProgramText>프로그램</ProgramText>
+      </Background>
     </div>
   );
 };
