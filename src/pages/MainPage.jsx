d2c4375edf8dc52261a39236859264da3d462ccf
osohyun0224 <osohyun0224@naver.com>
2023-08-24 02:09:30 +0900

    Feat: FilterButton 필터링화 구현을 위한 더미 태그 데이터 삽입

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -106,56 +106,56 @@ const MainPage = () => {
           title="거북목 탈출코스"
           description="이 코스는 목과 어깨의 근육을 이완시켜주는 운동을 포함하고 있습니다."
           time="총 10분"
-          tags={["테스트1", "운동"]}
+          tags={["목", "어깨", "앉은 자세"]}
         />
         <CourseCard
           image={null}
           title="코어 강화 코스"
           description="코어 근육을 강화하는데 초점을 둔 운동을 학습합니다."
           time="총 20분"
-          tags={["코어", "강화"]}
+          tags={["팔", "선 자세"]}
         />
         <CourseCard
           image={null}
           title="유연성 향상 코스"
           description="몸의 유연성을 높이는 스트레칭 운동을 포함하고 있습니다."
           time="총 15분"
-          tags={["스트레칭", "유연성"]}
+          tags={["어깨", "앉은 자세"]}
         />
         <CourseCard
           image={null}
           title="하체 강화 코스"
           description="다리와 엉덩이 근육을 강화하는 운동을 진행합니다."
           time="총 25분"
-          tags={["하체", "강화"]}
+          tags={["다리", "선 자세"]}
         />
         <CourseCard
           image={null}
           title="유산소 운동 코스"
           description="심장 건강과 체력 향상을 위한 유산소 운동을 합니다."
           time="총 30분"
-          tags={["유산소", "체력"]}
+          tags={["어깨", "선 자세"]}
         />
         <CourseCard
           image={null}
           title="근력 운동 코스"
           description="체중을 이용한 근력 운동을 중점적으로 합니다."
           time="총 20분"
-          tags={["근력", "체중운동"]}
+          tags={["허벅지", "앉은 자세"]}
         />
         <CourseCard
           image={null}
           title="밸런스 트레이닝"
           description="몸의 균형 능력을 향상시키기 위한 운동 코스입니다."
           time="총 20분"
-          tags={["밸런스", "균형"]}
+          tags={["어깨", "앉은 자세"]}
         />
         <CourseCard
           image={null}
           title="포스쳐 교정 코스"
           description="올바른 자세를 유지하기 위한 교정 운동을 포함하고 있습니다."
           time="총 15분"
-          tags={["자세", "교정"]}
+          tags={["목", "선 자세"]}
         />
       </CardContainer>
     </div>
