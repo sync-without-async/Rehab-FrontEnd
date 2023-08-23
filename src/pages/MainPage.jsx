c853529db7dd952f1f9c9fd4643403b0946f85ce
osohyun0224 <osohyun0224@naver.com>
2023-08-24 01:19:16 +0900

    Feat: 메인페이지 카테고리 / 자세별 필터 버튼 props까지 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'; 
 import Header from "../components/Header";
 import styled from 'styled-components';
 import dumbbell from "../assets/images/dumbbell.png";
@@ -8,6 +9,11 @@ const Background = styled.div`
   background-color: #14F2C6;
   position: relative;
   margin-top: 20px;
+`;
+
+const Label = styled.span`
+  display: inline-block;
+  width: 110px; 
 `;
 
 const DumbbellImage = styled.img`
@@ -23,7 +29,36 @@ const ProgramText = styled.p`
   left: 118px;
 `;
 
+const CategoryText = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 400px;
+  margin-top: 20px;
+`;
+
+const FilterButton = styled.button`
+  width: 130px;
+  height: 40px;
+  border-radius: 10px;
+  border: ${props => props.selected ? 'none' : '1px solid black'};  
+  background-color: ${props => props.selected ? '#ACEAFF' : 'white'};
+  color: ${props => props.selected ? 'black' : 'black'}; 
+  margin-left: 10px;  
+  transition: 0.3s;
+
+  &:first-child {
+    margin-left: 0;  
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
 const MainPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedPosture, setSelectedPosture] = useState(null);
+
   return (
     <div>
       <Header />
@@ -31,8 +66,37 @@ const MainPage = () => {
         <DumbbellImage src={dumbbell} alt="Dumbbell" />
         <ProgramText>프로그램</ProgramText>
       </Background>
+      <CategoryText>
+        <Label>카테고리별</Label>
+        {['팔', '어깨', '목', '허벅지'].map(category => (
+          <FilterButton
+            key={category}
+            selected={selectedCategory === category}
+            onClick={() => setSelectedCategory(category)} 
+          >
+            {category}
+          </FilterButton>
+        ))}
+      </CategoryText>
+      <CategoryText>
+        <Label>자세</Label>
+        {['선 자세', '앉은 자세', '누운 자세'].map(posture => (
+          <FilterButton
+            key={posture}
+            selected={selectedPosture === posture}
+            onClick={() => setSelectedPosture(posture)} 
+          >
+            {posture}
+          </FilterButton>
+        ))}
+      </CategoryText>
     </div>
   );
 };
 
 export default MainPage;
+
+
+
+
+
