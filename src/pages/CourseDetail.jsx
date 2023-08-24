d9b009779045b3ea4e7b64b315dc5e73feee14fc
osohyun0224 <osohyun0224@naver.com>
2023-08-24 15:31:42 +0900

    Feat: 운동 상세페이지 전체 구현

--- src/pages/CourseDetail.jsx
+++ src/pages/CourseDetail.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
-import styled from 'styled-components';
+import styled from "styled-components";
 import dumbbell from "../assets/images/dumbbell.png";
 import { getCourse } from "../librarys/exercise-api.js";
-import { useParams } from 'react-router-dom';
-
+import { useParams } from "react-router-dom";
+import CheckB from "../assets/images/Check-Before.png";
+import CheckA from "../assets/images/Check-After.png";
+import Player from "../assets/images/play.png";
 
 const Background = styled.div`
   width: 100%;
@@ -57,13 +59,83 @@ const DumbbellImage = styled.img`
   bottom: 10px;
 `;
 
+const CurriculumTitle = styled.h2`
+  margin-top: 20px;
+  margin-left: 80px;
+`;
+
+const CurriculumBox = styled.div`
+  margin-top: 25px;
+  margin-left: 80px;
+  width: 1200px;
+  height: 60px;
+  background-color: rgba(217, 217, 217, 0.38);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ActionTitleName = styled.span`
+  margin-left: 60px;
+`;
+
+const ActionTitleTime = styled.span`
+  margin-right: 80px;
+`;
+
+const DividerLine = styled.div`
+  margin-top: 10px;
+  margin-left: 80px;
+  width: 1200px;
+  height: 1px;
+  background-color: black;
+`;
+
+const CourseInfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 80px;
+  width: 1200px;
+  height: 60px;
+`;
+
+const CheckImage = styled.img`
+  width: 25px;
+  height: 25px; 
+  margin-left: 10px;
+`;
+
+const ActionName =styled.span`
+margin-left: -865px;
+`
+const ActionTime =styled.span`
+margin-right: -860px;
+`
+
+const PlayerButton = styled.button`
+  background: url(${Player}) no-repeat center center;
+  width: 40px;  
+  height: 40px;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  transition: 0.3s;
+  margin-right:15px;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
 const CourseDetail = () => {
   const [course, setCourse] = useState(null);
-  const { id } = useParams(); 
+  const [isChecked, setIsChecked] = useState(false); 
+  const { id } = useParams();
 
   useEffect(() => {
     async function fetchCourse() {
-      const courseData = await getCourse(Number(id)); 
+      const courseData = await getCourse(Number(id));
       setCourse(courseData);
     }
 
@@ -82,10 +154,28 @@ const CourseDetail = () => {
         <ExerciseTitle>{course.title}</ExerciseTitle>
         <ExerciseDescription>{course.description}</ExerciseDescription>
         {course.tags.map((tag, index) => (
-          <Tag key={index} style={{ left: `${500 + (110 * index)}px` }}>{tag}</Tag>
+          <Tag key={index} style={{ left: `${500 + 110 * index}px` }}>
+            {tag}
+          </Tag>
         ))}
         <DumbbellImage src={dumbbell} alt="Dumbbell" />
       </Background>
+      <CurriculumTitle>커리큘럼</CurriculumTitle>
+      <CurriculumBox>
+        <ActionTitleName>동작이름</ActionTitleName>
+        <ActionTitleTime>동작시간</ActionTitleTime>
+      </CurriculumBox>
+      <CourseInfoContainer>
+      <CheckImage 
+          src={isChecked ? CheckA : CheckB} 
+          alt={isChecked ? "Check After" : "Check Before"}
+          onClick={() => setIsChecked(prev => !prev)}
+        />
+        <ActionName>{course.title}</ActionName> 
+        <ActionTime>{course.time}</ActionTime> 
+        <PlayerButton onClick={() => {}} />
+      </CourseInfoContainer>
+      <DividerLine />
     </div>
   );
 };
