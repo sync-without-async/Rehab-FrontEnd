3e7e679359b65cc02306a14a5d70f7f66c91e410
osohyun0224 <osohyun0224@naver.com>
2023-08-24 14:28:47 +0900

    Feat: 운동 세부 페이지 수강 정보 구현

--- src/pages/CourseDetail.jsx
+++ src/pages/CourseDetail.jsx
@@ -1,81 +1,88 @@
+import { useState, useEffect } from 'react';
 import Header from "../components/Header";
 import styled from 'styled-components';
+import dumbbell from "../assets/images/dumbbell.png";
+import { getCourse } from "../librarys/exercise-api.js";
 
-const CourseBox = styled.div`
-  width: 1100px;
-  height: 216px;
-  background-color: #D9D9D9;
-  display: flex;
-  flex-direction: column;
+const Background = styled.div`
+  width: 100%;
+  height: 250px;
+  background-color: #14f2c6;
+  position: relative;
+  margin-top: 20px;
+`;
+
+const ExerciseImage = styled.img`
+  position: absolute;
+  left: 150px;
+  top: 40px;
+  width: 300px;
+  height: 180px;
+  object-fit: cover;
+`;
+
+const ExerciseTitle = styled.h1`
+  position: absolute;
+  left: 500px;
+  top: 40px;
+  font-weight: bold;
+`;
+
+const ExerciseDescription = styled.p`
+  position: absolute;
+  left: 500px;
+  top: 70px;
+`;
+
+const Tag = styled.div`
+  position: absolute;
+  left: 500px;
+  top: 120px;
+  width: 100px;
+  height: 30px;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 20px;
+  display: inline-flex;
   justify-content: center;
   align-items: center;
-  margin-left: 120px;
-  margin-top:30px;
+  margin-right: 10px;
 `;
 
-const CourseTitle = styled.h1`
-  font-size: 24px;
-  font-weight: bold;
-  margin-top: -110px;
-`;
-
-const LearningStatus = styled.div`
-  width: 1112px;
-  display: flex;
-  flex-direction: column;
-  margin-left: 120px;
-`;
-
-const LearningTitle = styled.h2`
-  font-size: 20px;
-  font-weight: bold;
-  margin-top: 20px;
-  margin-bottom:5px;
-`;
-
-const LearningLine = styled.hr`
-  width: 1100px;
-  border: 2px solid black;
-  margin-bottom:5px;
-`;
-
-const ProgressWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const ProgressText = styled.span`
-  font-size: 18px;
-  margin-right: 40px;
-`;
-
-const ProgressBar = styled.div`
-  width: 800px;
-  height: 40px;
-  background-color: grey; 
-`;
-
-const ProgressCount = styled.span`
-  font-size: 18px;
-  margin-left: 30px;
+const DumbbellImage = styled.img`
+  position: absolute;
+  right: 100px;
+  bottom: 10px;
 `;
 
 const CourseDetail = () => {
+  const [course, setCourse] = useState(null);
+
+  useEffect(() => {
+    async function fetchCourse() {
+      const courseData = await getCourse(1);
+      setCourse(courseData);
+    }
+
+    fetchCourse();
+  }, []);
+
+  if (!course) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <Header />
-      <CourseBox>
-        <CourseTitle>[맨몸운동] 거북목 탈출코스</CourseTitle>
-      </CourseBox>
-      <LearningStatus>
-        <LearningTitle>학습현황</LearningTitle>
-        <LearningLine />
-        <ProgressWrapper>
-          <ProgressText>나의 진도율</ProgressText>
-          <ProgressBar />
-          <ProgressCount>0/ 총개수</ProgressCount>
-        </ProgressWrapper>
-      </LearningStatus>
+      <Background>
+        <ExerciseImage src={course.image} alt={course.title} />
+        <ExerciseTitle>{course.title}</ExerciseTitle>
+        <ExerciseDescription>{course.description}</ExerciseDescription>
+        {course.tags.map((tag, index) => (
+          <Tag key={index} style={{ left: `${500 + (110 * index)}px` }}>{tag}</Tag>
+        ))}
+        <DumbbellImage src={dumbbell} alt="Dumbbell" />
+      </Background>
     </div>
   );
 };
