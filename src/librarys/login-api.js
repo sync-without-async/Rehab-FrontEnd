a29d5c4ff381af1d86c56f7b04ebf61f682b6cb3
osohyun0224 <osohyun0224@naver.com>
2023-08-28 23:56:28 +0900

    !HOTFIX: courseList에서도 해당 ID의 코스를 찾을 수 있도록함

--- src/librarys/login-api.js
+++ src/librarys/login-api.js
@@ -1,64 +1,31 @@
-const accounts = [
-  {
-    id: "admin@example.com",
-    password: "qwerty123",
-    name: "관리자",
-    admin: true,
-  },
-  {
-    id: "user@example.com",
-    password: "qwerty123",
-    name:"일반유저",
-    admin: false,
-  },
-];
+export async function userLogin(id, password) {
+  const accounts = [
+    {
+      id: "admin@example.com",
+      password: "qwerty123",
+      name: "관리자",
+      admin: true,
+    },
+    {
+      id: "user@example.com",
+      password: "qwerty123",
+      name:"일반유저",
+      admin: false,
+    },
+  ];
 
-async function userLogin(mid, password) {
-  const account = accounts.find(account => account.id === mid && account.password === password);
-  
+  const account = accounts.find((item) => item.id === id);
   if (!account) {
-      console.error("유효하지 않은 아이디와 비밀번호 입니다.");
-      return null;
+    return null;
   }
-
-  const apiUrl = "/login";
-
-  try {
-      const response = await fetch(apiUrl, {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-              mid: mid,
-              password: password,
-          }),
-      });
-
-      if (!response.ok) {
-          const errorData = await response.json();
-          console.error(`API call failed with status: ${response.status}, Message: ${errorData.message}`);
-          throw new Error(`API call failed with status: ${response.status}`);
-      }
-
-      const responseData = await response.json();
-      const loginResult = {
-          email: mid,
-          name: account.name,
-          access_token: responseData.accessToken,
-          refresh_token: responseData.refreshToken,
-          admin: account.admin,
-      };
-      
-      // localStorage에 사용자 정보 저장
-      localStorage.setItem('user', JSON.stringify(loginResult));
-      
-      return loginResult;
-
-  } catch (error) {
-      console.error("Login failed:", error);
-      return null;
+  if (account.password !== password) {
+    return null;
   }
+  return {
+    email: account.id,
+    name: account.name,
+    access_token: "token1",
+    refresh_token: "token2",
+    admin: account.admin,
+  };
 }
-
-export { userLogin };
