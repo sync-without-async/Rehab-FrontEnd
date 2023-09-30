f298cc0ea9dde556061b149893da0fef5b719999
osohyun0224 <osohyun0224@naver.com>
2023-09-30 18:28:23 +0900

    Feat: 일단 날짜 선택 뜨는 거 성공했따~!~!~!

--- src/components/Input/DateSelect.jsx
+++ src/components/Input/DateSelect.jsx
@@ -1,22 +1,83 @@
-import dayjs from 'dayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import React from 'react';
+import { BsFillCalendarHeartFill } from 'react-icons/bs';
+import {useState} from 'react';
+import Datetime from 'react-datetime';
 
 const DateSelect = () => {
-  const [value, setValue] = React.useState(dayjs('2022-04-17'));
+  const [date, setDate] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const format = 'YYYY-MM-DD';
+
+  const handleClickButton = () => {
+    setOpen(!open);
+  };
+
+  const handleChangeCalendar = (selected) => {
+    const formattedDate = selected.format(format);
+    setDate(formattedDate);
+    setOpen(false);
+  };
+
+  const getSeparator = () => {
+    const regex = /[^0-9a-zA-Z]+/;
+    const match = format.match(regex);
+
+    if (match) {
+      const symbol = match[0];
+      const indexes = [];
+
+      for (let i = 0; i < format.length; i++) {
+        if (format[i] === symbol) {
+          indexes.push(i);
+        }
+      }
+
+      return { symbol, indexes };
+    }
+    return { symbol: undefined, indexes: [] };
+  };
+
+  const separator = getSeparator();
+
+  const handleChangeDate = (e) => {
+    let currentDate = e.target.value;
+
+    if (separator.symbol && separator.indexes.length > 0) {
+      separator.indexes.forEach((index) => {
+        if (currentDate.length > index) {
+          currentDate =
+            currentDate.slice(0, index) +
+            separator.symbol +
+            currentDate.slice(index);
+        }
+      });
+    }
+
+    setDate(currentDate);
+  };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
-        format="YYYY-MM-DD"
-        showDaysOutsideCurrentMonth
-        value={value}
-        onChange={(newValue) => setValue(newValue)}
+    <div>
+      <input
+        type='text'
+        value={date}
+        placeholder='placeholder'
+        onChange={handleChangeDate}
       />
-    </LocalizationProvider>
+      <button type='button' onClick={handleClickButton}>
+        <BsFillCalendarHeartFill />
+      </button>
+      {open && (
+          <Datetime
+            input={false}
+            timeFormat={false}
+            dateFormat={format}
+            value={date}
+            onChange={handleChangeCalendar}
+          />
+      )}
+    </div>
   );
-}
+};
 
 export default DateSelect;
