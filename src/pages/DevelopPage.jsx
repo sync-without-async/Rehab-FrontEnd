3f8748e7a971870f34e7c7340f4137249a14f0b1
osohyun0224 <osohyun0224@naver.com>
2023-10-01 16:54:45 +0900

    Feat: / 에 다른 페이지들 확인할 수 있도록 버튼 라우트 구현

--- src/pages/DevelopPage.jsx
+++ src/pages/DevelopPage.jsx
@@ -1,6 +1,6 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../components/Header/Header';
-import UserUntactRecord from '../components/UserDashBoard/UserUntactRecord';
 
 const PageContainer = styled.div`
   display: flex;
@@ -13,18 +13,51 @@ const CenteredContainer = styled.div`
   justify-content: center;
   align-items: center;
   flex: 1; 
-  margin-top: -50px;
+  flex-direction: column;
+  gap: 20px;  
+`;
+
+const RouteButton = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border: none;
+  background-color: #3592FF;
+  color: white;
+  border-radius: 5px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #2c75e0;
+  }
 `;
 
 const DevelopPage = () => {
+    const routes = [
+      { path: "/login", name: "030LoginPage" },
+      { path: "/signup", name: "017SignupPage" },
+      { path: "/", name: "DevelopPage" },
+      { path: "/userdash", name: "001MyUserPage" },
+      { path: "/useruntactreserve", name: "004UserUntactReservePage" },
+      { path: "/userreserve", name: "005UserReservePage" },
+      { path: "/doctordash", name: "009DoctorDashBoardPage" },
+      { path: "/doctorchart", name: "010DoctorChartPage" },
+      { path: "/doctordetail", name: "013DoctorDetailPage" },
+      { path: "/doctorpatientlist", name: "012DoctorPatientListPage" },
+      { path: "/doctoruntactreserve", name: "014DoctorUntactReservePage" },
+    ];
 
   return (
-      <PageContainer>
-          <Header/>
-          <CenteredContainer>
-            <UserUntactRecord />
-          </CenteredContainer>
-      </PageContainer>
+    <PageContainer>
+      <Header />
+      <CenteredContainer>
+        {routes.map(route => (
+          <Link key={route.path} to={route.path}>
+            <RouteButton>{route.name}</RouteButton>
+          </Link>
+        ))}
+      </CenteredContainer>
+    </PageContainer>
   );
 }
 
