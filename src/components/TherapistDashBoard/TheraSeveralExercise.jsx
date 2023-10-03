a74d6a2f4117c16f987774e949c950c4ec8acf5f
osohyun0224 <osohyun0224@naver.com>
2023-10-03 12:44:23 +0900

    Feat: 가이드 영상 모달 컴포넌트 구현

--- src/components/TherapistDashBoard/TheraSeveralExercise.jsx
+++ src/components/TherapistDashBoard/TheraSeveralExercise.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import TheraExerciseModal from './TheraExerciseModal';
+import {useState} from "react";
 
 const CardContainer = styled.div`
   display: flex;
@@ -31,19 +33,30 @@ const ExerciseTitle = styled.div`
 const ExerciseDescription = styled.div`
   font-size: 13px;
   color: #000000;
-  margin-top: 5px; // 제목과 설명 텍스트 사이의 간격을 위해
+  margin-top: 5px; 
 `;
 
 const TheraSeveralExercise = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  }
+
   return (
     <>
-      <CardContainer>
+      <CardContainer onClick={handleOpenModal}>
         <ExerciseThumbnail />
         <div>
           <ExerciseTitle>운동 제목</ExerciseTitle>
           <ExerciseDescription>운동 설명</ExerciseDescription>
         </div>
       </CardContainer>
+      {isModalOpen && <TheraExerciseModal onClose={handleCloseModal} />}
     </>
   );
 };
