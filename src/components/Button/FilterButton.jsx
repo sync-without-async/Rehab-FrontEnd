f3d7f9923a653fb8acf59c808594e705288d8081
osohyun0224 <osohyun0224@naver.com>
2023-10-03 13:39:16 +0900

    Feat: 운동 등록 페이지 구현

--- src/components/Button/FilterButton.jsx
+++ src/components/Button/FilterButton.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
+  margin-left: -40px;
 `;
 
 const Label = styled.label`
--- src/components/TherapistDashBoard/TheraExerciseAdd.jsx
+++ src/components/TherapistDashBoard/TheraExerciseAdd.jsx
@@ -2,10 +2,11 @@ import styled from "styled-components";
 import TheraVideoUploader from "./TheraVideoUploader";
 import InputText from "../Input/InputText";
 import InputDText from "../Input/InputDText";
+import FilterButton from "../Button/FilterButton";
 
 const Container = styled.div`
   width: 800px;
-  height: 1200px;
+  height: 800px;
   margin: 0 auto;
   padding: 20px;
   border: 1px solid #0064ff;
@@ -36,6 +37,30 @@ const TitleA = styled.p`
   margin-bottom:10px;
 `;
 
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  margin-top:20px;
+`;
+
+const Button = styled.button`
+  width: 210px;
+  height: 40px;
+  background-color: #3592FF;
+  font-weight: 300;
+  color: #FEFDFD;
+  font-size: 14px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top:20px;
+  display: block;     
+  margin-left: auto;
+  margin-right: auto; 
+`;
+
 const TheraExerciseAdd = () => {
   return (
     <Container>
@@ -43,8 +68,12 @@ const TheraExerciseAdd = () => {
       <Divider />
       <TitleA>가이드 영상 업로드*</TitleA>
       <TheraVideoUploader />
-      <InputText label="운동 제목 *" />
+      <FlexContainer>
+        <InputText label="운동 제목 *" />
+        <FilterButton label="운동 태그 *" />
+      </FlexContainer>
       <InputDText label="운동 설명 *"/>
+      <Button>기록 추가</Button>
     </Container>
   );
 };
