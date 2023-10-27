3fe0501ee8f8d3a3421b8a894c06274ada06961e
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-10-27 18:52:19 +0900

    Feat: InputArea 컴포넌트 추가

---
+++ src/components/Input/InputArea.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const Item = styled.textarea`
+  width: 240px;
+  height: 107px;
+  padding: 12px;
+  font-size: 14px;
+  border-radius: 10px;
+  background-color: #fafafa;
+  border: 1px solid #bbbbbb;
+  padding-left: 12px;
+  resize: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+function InputArea({ value, onInput, className }) {
+  return (
+    <Item type="text" value={value} onInput={onInput} className={className} />
+  );
+}
+
+InputArea.propTypes = {
+  value: PropTypes.string,
+  onInput: PropTypes.func,
+  className: PropTypes.string,
+};
+
+export default InputArea;
