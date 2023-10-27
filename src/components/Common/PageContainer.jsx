b2896d2829e623dd0a16a93684b488b92b2f57d4
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-10-27 17:24:50 +0900

    Fix: PageContainer에 화면을 넉넉하게 볼 수 있도록 padding 추가

--- src/components/Common/PageContainer.jsx
+++ src/components/Common/PageContainer.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const PageContainer = styled.div`
   margin-top: 60px;
+  padding-bottom: 120px;
   display: flex;
   flex-direction: column;
   align-items: center;
