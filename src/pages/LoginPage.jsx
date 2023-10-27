042c58c28115339955f6b2f143449745f58ebdf1
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-10-27 17:23:54 +0900

    Feat: 로그인 페이지 미사용 함수 제거

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,16 +3,9 @@ import { useState } from "react";
 import PageContainer from "../components/Common/PageContainer.jsx";
 
 const LoginPage = () => {
-  const [userType, setUserType] = useState(null); // 유저 타입 상태 추가
-
-  // 로그인 성공시 호출되는 함수
-  const handleLoginSuccess = (response) => {
-    setUserType(response.type);
-  };
-
   return (
     <PageContainer>
-      <LoginComponents onLoginSuccess={handleLoginSuccess} />
+      <LoginComponents />
     </PageContainer>
   );
 };
