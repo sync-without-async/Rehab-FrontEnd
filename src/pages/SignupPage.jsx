4057635d1475d038bdfe03b9115478d6a9cba19b
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-10-31 16:32:49 +0900

    refactor: 로그인 페이지 구조 정리

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -4,23 +4,12 @@ import SignupComponents from "../components/Accounts/SignupComponents";
 const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
-`;
-
-const CenteredContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-  margin-top: -50px;
 `;
 
 const SignupPage = () => {
   return (
     <PageContainer>
-      <CenteredContainer>
-        <SignupComponents />
-      </CenteredContainer>
+      <SignupComponents />
     </PageContainer>
   );
 };
