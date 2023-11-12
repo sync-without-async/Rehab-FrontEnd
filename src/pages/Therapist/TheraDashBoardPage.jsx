66e31bd62b595f79b178beca9d474486159c2629
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-11-12 17:43:19 +0900

    Refactor: 대시보드 페이지 컴포넌트 리팩토링

--- src/pages/Therapist/TheraDashBoardPage.jsx
+++ src/pages/Therapist/TheraDashBoardPage.jsx
@@ -1,21 +1,12 @@
 import styled from "styled-components";
-import TheraDashHeader from "../../components/TherapistDashBoard/TheraDashHeader";
-import CardhButton from "../../components/Button/CardButton";
+import CardButton from "../../components/Button/CardButton";
 import ReservationMiniList from "../../components/Reservation/ReservationMiniList.jsx";
+import { MdDirectionsRun, MdVideoChat, MdAssignment } from "react-icons/md";
+import PageContainer from "../../components/Common/PageContainer.jsx";
+import EmployeeHeader from "../../components/Dashboard/EmployeeHeader.jsx";
 
-const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-`;
-
-const CenteredContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-  margin-top: 50px;
+const Container = styled(PageContainer)`
+  gap: 28px;
 `;
 
 const CardButtonGroup = styled.div`
@@ -23,23 +14,41 @@ const CardButtonGroup = styled.div`
   justify-content: center;
   align-items: center;
   gap: 40px;
-  margin-top: 20px;
-  margin-bottom: 20px;
 `;
+
+const buttons = [
+  {
+    icon: <MdDirectionsRun />,
+    title: "운동 목록",
+    description: "등록된 운동 목록을 둘러보거나,\n새로운 운동을 등록합니다.",
+    link: "/theraexerciselist",
+  },
+  {
+    icon: <MdVideoChat />,
+    title: "실시간 비대면 진료",
+    description: "담당 환자와 실시간 비대면\n진료를 진행합니다.",
+    link: "/untact/list",
+  },
+  {
+    icon: <MdAssignment />,
+    title: "환자 목록",
+    description:
+      "담당 환자들의 차트를 둘러보거나\n환자에게 재활 프로그램을 할당합니다.",
+    link: "/therapatientlist",
+  },
+];
 
 const TheraDashBoardPage = () => {
   return (
-    <PageContainer>
-      <CenteredContainer>
-        <TheraDashHeader />
-        <CardButtonGroup>
-          <CardhButton mode="exercise" />
-          <CardhButton mode="treatment" />
-          <CardhButton mode="add" />
-        </CardButtonGroup>
-        <ReservationMiniList />
-      </CenteredContainer>
-    </PageContainer>
+    <Container>
+      <EmployeeHeader />
+      <CardButtonGroup>
+        {buttons.map((item, index) => (
+          <CardButton key={index} {...item} />
+        ))}
+      </CardButtonGroup>
+      <ReservationMiniList />
+    </Container>
   );
 };
 
