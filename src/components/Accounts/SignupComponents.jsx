e712ff37427bb155d6ad0b9f6e87b1fc9177d446
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-11-13 19:03:49 +0900

    Refactor: 회원가입 페이지 리팩토링

--- src/components/Accounts/SignupComponents.jsx
+++ src/components/Accounts/SignupComponents.jsx
@@ -1,167 +1,48 @@
 import styled from "styled-components";
-import RoleButton from "../Button/RoleButton";
-import Dropdown from "../Dropdown/Dropdown";
-import InputText from "../Input/InputText";
-import InputImage from "../Input/InputImage";
+import BlockContainer from "../Common/BlockContainer.jsx";
+import TitleText from "../Common/TitleText.jsx";
+import RoleButton from "../Button/RoleButton.jsx";
+import Dropdown from "../Dropdown/Dropdown.jsx";
+import InputImage from "../Input/InputImage.jsx";
+import InputTextContainer from "../Input/InputTextContainer.jsx";
+import Button from "../Button/Button.jsx";
+import DropdownFilter from "../Dropdown/DropdownFilter.jsx";
 
-const SignupContainer = styled.div`
-  width: 750px;
-  height: 750px;
-  border-radius: 7.5px;
-  background-color: #ffffff;
-  border: 1.5px solid #0064ff;
-  position: relative;
-  box-shadow: 0px 9px 18px rgba(0, 0, 0, 0.1);
+const Grid = styled.div`
+  margin: 48px 70px;
+  display: grid;
+  grid-template-columns: 240px 240px;
+  gap: 28px 100px;
 `;
 
-const Divider = styled.div`
-  width: 675px;
-  height: 0.75px;
-  background-color: #d9d9d9;
-  margin-top: 7.5px;
-  margin-left: 22.5px;
-  margin-right: auto;
+const RegisterButton = styled(Button)`
+  margin-top: 4px;
+  grid-column-end: span 2;
+  justify-self: center;
 `;
 
-const Title = styled.h1`
-  font-size: 26.25px;
-  font-family: "Spoqa Han Sans Neo", "sans-serif";
-  font-weight: 700;
-  margin-left: 22.5px;
-  margin-top: 18px;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 580px;
-  margin-top: 60px;
-  margin-left: 97.5px;
-  gap: 40px;
-`;
-
-const DropdownsContainer = styled.div`
-  margin-top: 22.5px;
-  margin-left: 97.5px;
-`;
-
-const InputFieldsContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 22.5px;
-  margin-left: 97.5px;
-  gap: 15px;
-  width: 525px;
-`;
-
-const FlexContainer = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  margin-left: 0px;
-  gap: 70px;
-  font-size: 16px;
-`;
-
-const StyledInputImage = styled(InputImage)``;
-
-const EmailInputContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 3.75px;
-  width: 525px;
-  margin-left: 97.5px;
-  margin-top: 22.5px;
-`;
-
-const EmailInput = styled(InputText)`
-  width: 150%;
-`;
-
-const VerifyButton = styled.button`
-  width: 97.5px;
-  height: 37.5px;
-  background-color: #f0f0f0;
-  border: 0.75px solid #bbbbbb;
-  border-radius: 7.5px;
-  font-family: "Spoqa Han Sans Neo", "sans-serif";
-  font-size: 12px;
-  font-weight: 700;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-
-  &:focus {
-    outline: none;
-  }
-`;
-
-const Button = styled.button`
-  width: 210px;
-  height: 45px;
-  background-color: #3592ff;
-  border-radius: 7.5px;
-  color: white;
-  font-size: 16.5px;
-  border: none;
-  cursor: pointer;
-  position: absolute;
-  font-family: "Spoqa Han Sans Neo", "sans-serif";
-  margin-top: 30px;
-  left: 0;
-  right: 0;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const SignupComponents = () => {
-  // 소속병원 선택
-  const hospitals = [
-    "춘천성심병원",
-    "강남성심병원",
-    "동탄성심병원",
-    "한림성심병원",
-    "한강성심병원",
-    "강동성심병원",
-  ];
-
-  // 재활 분야 선택
-  const fields = ["재활의학과", "신경외과", "정형외과"];
-
+const Signup = () => {
   return (
-    <SignupContainer>
-      <Title>회원가입</Title>
-      <Divider />
-      <ButtonContainer>
+    <BlockContainer>
+      <TitleText text="회원가입" />
+      <Grid>
         <RoleButton role="doctor" />
         <RoleButton role="therapist" />
-      </ButtonContainer>
-      <FlexContainer>
-        <DropdownsContainer>
-          <Dropdown label="소속 병원명 *" items={hospitals} />
-          <Dropdown label="재활 분야 *" items={fields} />
-        </DropdownsContainer>
-        <StyledInputImage />
-      </FlexContainer>
-      <InputFieldsContainer>
-        <InputText label="성함 *" />
-        <InputText label="연락처 *" />
-      </InputFieldsContainer>
-      <EmailInputContainer>
-        <InputText label="이메일 *">
-          <EmailInput type="text" />
-        </InputText>
-        <VerifyButton>인증</VerifyButton>
-      </EmailInputContainer>
-      <InputFieldsContainer>
-        <InputText label="아이디 *" />
-        <InputText label="비밀번호 *" />
-      </InputFieldsContainer>
-      <Button>가입하기</Button>
-    </SignupContainer>
+        <DropdownFilter label="소속 병원명 *" items={[]} />
+        <InputImage style={{ gridRowEnd: "span 2" }} />
+        <DropdownFilter label="재활 분야 *" items={[]} />
+        <InputTextContainer label="성함 *" />
+        <InputTextContainer label="연락처 *" />
+        <InputTextContainer
+          label="이메일 *"
+          style={{ gridColumnEnd: "span 2" }}
+        />
+        <InputTextContainer label="인증번호 *" />
+        <InputTextContainer label="비밀번호 *" />
+        <RegisterButton type="primary">회원가입</RegisterButton>
+      </Grid>
+    </BlockContainer>
   );
 };
 
-export default SignupComponents;
+export default Signup;
