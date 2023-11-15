7119a03ccad301e2b462e4830c75b1b5d2aae0ad
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-11-15 19:29:34 +0900

    Feat: App.jsx에 권한에 따른 라우팅 기능 추가

---
+++ src/components/Route/AuthorizedRoute.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import { Navigate, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectRole } from "../../redux/userSlice.js";
+import { ROLE_TYPE } from "../../librarys/type.js";
+import { useMemo } from "react";
+
+const AuthorizedRoute = ({ children, to, whitelist }) => {
+  const userRole = useSelector(selectRole);
+  const pathname = useLocation().pathname;
+  const redirect = to || pathname;
+
+  const url = useMemo(() => {
+    if (userRole === ROLE_TYPE.VISITOR) {
+      return "/login?redirect=" + redirect;
+    } else {
+      return "/dashboard";
+    }
+  }, [userRole]);
+
+  const authorized = whitelist === null || whitelist.includes(userRole);
+
+  return authorized ? <>{children}</> : <Navigate to={url} />;
+};
+
+AuthorizedRoute.propTypes = {
+  children: PropTypes.node,
+  whitelist: PropTypes.arrayOf(PropTypes.number),
+  to: PropTypes.string,
+};
+
+AuthorizedRoute.defaultProps = {
+  to: null,
+  whitelist: null,
+};
+
+export default AuthorizedRoute;
