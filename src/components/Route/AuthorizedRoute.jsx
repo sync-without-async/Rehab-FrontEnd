3f6d8a48314c955ef53dfe3350c5bb9270084606
PortalCube <35104213+PortalCube@users.noreply.github.com>
2023-11-15 19:31:09 +0900

    Chore: AuthorizedRoute 일시 비활성화

--- src/components/Route/AuthorizedRoute.jsx
+++ src/components/Route/AuthorizedRoute.jsx
@@ -18,7 +18,9 @@ const AuthorizedRoute = ({ children, to, whitelist }) => {
     }
   }, [userRole]);
 
-  const authorized = whitelist === null || whitelist.includes(userRole);
+  const authorized = true; // For testing only
+
+  // const authorized = whitelist === null || whitelist.includes(userRole);
 
   return authorized ? <>{children}</> : <Navigate to={url} />;
 };
