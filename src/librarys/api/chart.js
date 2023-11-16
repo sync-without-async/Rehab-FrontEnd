180ededa33e5b376a10e7201fddfcf81acc669d9
osohyun0224 <osohyun0224@naver.com>
2023-11-16 12:24:49 +0900

    ✨ feat(Chart.js): 재활치료사 목록 조회 api 연결 함수 구현

--- src/librarys/api/chart.js
+++ src/librarys/api/chart.js
@@ -6,3 +6,10 @@ export async function registerChart(data, token) {
   const response = await axios.post("/chart/auth/register", data);
   return response.data;
 }
+
+export async function getTherapistList(token) {
+  const axios = getSpringAxios(token);
+
+  const response = await axios.get("/auth/getTherapistList");
+  return response.data;
+}
