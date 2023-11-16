80dd0a68d83d5b69c30363b02da8faa734ce134b
osohyun0224 <osohyun0224@naver.com>
2023-11-16 11:39:36 +0900

    ✨ feat(DoctorChart): 환자 차트 등록 시, 인증 토큰 구현
    - 인증 토큰을 사용하여 registerChart 함수를 수정하였습니다.

--- src/librarys/api/chart.js
+++ src/librarys/api/chart.js
@@ -1,7 +1,7 @@
 import { getSpringAxios } from "./axios";
 
-export async function registerChart(data) {
-  const axios = getSpringAxios();
+export async function registerChart(data, token) {
+  const axios = getSpringAxios(token); 
 
   const response = await axios.post("/chart/auth/register", data);
   return response.data;
